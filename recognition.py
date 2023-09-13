6788ba0cb4b4da06c4cb102b12741394273f3d24
Lereko Qholosha <lerekoqholosha9@gmail.com>
2023-09-13 15:44:01 +0200

    Update recognition.py

--- recognition.py
+++ recognition.py
@@ -1,4 +1,3 @@
-import tensorflow as tf
 import streamlit as st
 import cv2
 import numpy as np
@@ -7,35 +6,13 @@ import gdown
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from tensorflow.keras.models import load_model
-import numpy as np
-import matplotlib.pyplot as plt
-from keras.preprocessing import image
 # Authenticate with Google Drive API
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaIoBaseDownload
-import io
-import tensorflow as tf
-
-# Authenticate with Google Drive
-# Replace with your own credentials and file ID
-CLIENT_ID = '115991188667805246382'
-CLIENT_SECRET = '73f4ec0eb233922c63bc320c353533c817d0fcf8'
-FILE_ID = '1-34mfUqojaxl16p4LKQGQ-KSP7IqeJI5'
-
-# Create a Google Drive API service
-drive_service = build('drive', 'v3')
-credentials = None  # You should load your credentials here
-
-# Download the model file from Google Drive
-request = drive_service.files().get_media(fileId=FILE_ID)
-file_stream = io.BytesIO()
-downloader = MediaIoBaseDownload(file_stream, request)
-done = False
-while done is False:
-    status, done = downloader.next_chunk()
-
-# Load the model
-model = tf.keras.models.load_model(io.BytesIO(file_stream.getvalue()))
+try:
+    model_path = r"https://drive.google.com/file/d/1-34mfUqojaxl16p4LKQGQ-KSP7IqeJI5/my data/model.h5"
+    model = load_model(model_path)
+except:
+   
+    print("...it seems to be better to use more simple naming with the .h5 file!")
 
 
 # Set the title and description of the app
@@ -84,44 +61,35 @@ if st.button("Detect Objects"):
     image_path = "captured_image.jpg" if option == "Capture Image" else "uploaded_image.jpg"
     st.write("Performing object detection...")
 
-    ##test
-    # Get the classes
-classes =['Hand-bags', 'wallets', 'Bags', 'Balls', 'Sunglasses', 'shorts', 'Pants', 'T-shirts', 'sneakers', 'Loafers', 'Socks', 'Watches', 'Sandals']
-
-# Load the image for detection
-image_path = "path/to/image.jpg"
-image = cv2.imread(image_path)
-
-
-#preprocess
-def load_image(path):
-    img = image.load_img(path, target_size=model.input_shape[1:3])
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    return img, x
+    # Load YOLOv3 model
+    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+    #model= model 
+    with open("coco.names", "r") as f:
+        classes = f.read().strip().split("\n")
     
-
-# Predict the bounding boxes and classes for the image
-boxes, classes, scores =model = load_model(model_path)
-
-# Draw the bounding boxes and labels on the image
-for box, class_id, score in zip(boxes, classes, scores):
-    center_x, center_y, width, height = box.numpy()
-    x, y = int(center_x - width / 2), int(center_y - height / 2)
-    color = (0, 255, 0)
-    cv2.rectangle(image, (x, y), (x + width, y + height), color, 2)
-    label = f"{classes[class_id]}: {score:.2f}"
-    cv2.putText(image, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-# Display the image
-cv2.imshow("Object Detection", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-    ###end test
-   
+    # Load the image for detection
+    image = cv2.imread(image_path)
+    blob = cv2.dnn.blobFromImage(image, 1/255.0, (416, 416), swapRB=True, crop=False)
     
-st.image(image, caption="Detected Objects", use_column_width=True)
+    net.setInput(blob)
+    layer_names = net.getUnconnectedOutLayersNames()
+    outputs = net.forward(layer_names)
+
+    # Loop over each detection
+    for output in outputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x, center_y, width, height = list(map(int, detection[0:4] * np.array([image.shape[1], image.shape[0], image.shape[1], image.shape[0]])))
+                x, y = center_x - width // 2, center_y - height // 2
+                color = (0, 255, 0)
+                cv2.rectangle(image, (x, y), (x + width, y + height), color, 2)
+                label = f"{classes[class_id]}: {confidence:.2f}"
+                cv2.putText(image, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    
+    st.image(image, caption="Detected Objects", use_column_width=True)
 
 # Add image details and specify a directory to save the detected image
 if st.button("Add Image Details and Save"):
@@ -142,4 +110,3 @@ if st.button("Add Image Details and Save"):
         details_file.write(f"Image Description:\n{image_description}\n")
     
     st.success(f"Image and details saved in the '{save_dir}' directory.")
-
